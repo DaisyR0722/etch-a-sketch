22881b013c1eb9c7fc5e963dac5ee088e19cf00e
Daisy <156558580+DaisyR0722@users.noreply.github.com>
2026-06-06 12:58:25 -0500

    Implement 'createGrid' function to create grid squares

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function randomColor (){
     return color;
 }
 
+//Creating grid squares inside container.
+function createGrid(gridSize){
+    const squareSize = 500/gridSize
+for(let i=0; i < gridSize * gridSize; i++){
+    const square = document.createElement('div');
+    square.classList.add('grid-square')
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    //Set opacity to 10%
+    square.style.opacity = "0.1";
+    container.appendChild(square);
+    square.addEventListener('mouseover', ()=>{
+        square.style.backgroundColor = randomColor();
+        let currentOpacity = parseFloat(square.style.opacity);
+        if (currentOpacity < 1){
+            let newOpacity = (currentOpacity + 0.1);
+            square.style.opacity = newOpacity.toFixed(1);
+        }
+    })
+}
+}
+createGrid(16)
+
